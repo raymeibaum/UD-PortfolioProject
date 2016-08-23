76375b31fe19001775a0d2b0630676a20f2a8609
Ray Meibaum <rmeibaum@me.com>
2016-08-23 15:34:58 -0400

    Complete first draft for review

--- js/script.js
+++ js/script.js
@@ -2,77 +2,111 @@ $(function() {
 
   // Model
   var ray = {
-    about: "Ray Meibaum is pretty awesome.",
+    about: {
+      lead: "Ray Meibaum is a web developer who is ready to make a difference.",
+      main: "Although born and raised in St. Louis, Missouri, Ray is an " +
+        "Atlanta, Georgia based full-stack web developer who loves " +
+        "writing code not only for the excitement of accomplishment, but " +
+        "also for the profound effect programming can have on people all " +
+        "over the world. Web applications help billions of people every " +
+        "day in ways such as social networking and communications, " +
+        "banking and finance, news delivery, entertainment, and so much " +
+        "more. Ray is more than excited to live and work in an industry " +
+        "that is truly shaping the future of the human race."
+    },
     portrait: "img/IMG_4892.jpg",
-    portfolio: [
-      {
-        title: "Local Weather",
-        description: " the user to see pertinent weather information based on their location.",
-        features: ["Use of current location", "Google Reverse Geolocation API",
-                   "Forecast.io Weather API", "Skycons animated weather icons API"],
-        image: "img/LocalWeatherShowcase.png",
-        url: "https://raymeibaum.github.io/FCC-LocalWeather/"
-      },
-      {
-        title: "Random Quote Generator",
-        description: "Displays random programming related quotes to the user.",
-        features: [""],
-        image: "img/RandomQuoteGeneratorShowcase.png",
-        url: ""
-      },
-      {
-        title: "Twitch API",
-        description: "Uses the Twitch API to display an array of user statuses.",
-        features: [""],
-        image: "img/LocalWeatherShowcase.png",
-        url: ""
-      },
-      {
-        title: "Wikipedia Viewer",
-        description: "Allows the user to search for Wikipedia articles",
-        features: [""],
-        image: "img/WikipediaViewerShowcase.png",
-        url: ""
-      }
-    ],
-    skills: [
-      {
+    portfolio: [{
+      title: "Local Weather",
+      description: " the user to see pertinent weather information based on " +
+        "their location.",
+      features: [
+        "Use of current location",
+        "Google Reverse Geolocation API",
+        "Forecast.io Weather API",
+        "Skycons animated weather icons API"
+      ],
+      image: "img/LocalWeather.png",
+      url: "https://raymeibaum.github.io/FCC-LocalWeather/"
+    }, {
+      title: "Random Quote Generator",
+      description: "Displays random programming related quotes to the user.",
+      features: [
+        "Random programming quotes API by Adam Pope of Storm Consultancy",
+        "Twitter Tweet Button API",
+        "Bootstrap styling"
+      ],
+      image: "img/RandomQuoteGenerator.png",
+      url: "http://raymeibaum.github.io/FCC-RandomQuoteGenerator/"
+    }, {
+      title: "Twitch API",
+      description: "Uses the Twitch API to display an array of user statuses.",
+      features: [
+        "Twitch API",
+        "Icons by Font Awesome",
+        "Bootstrap styling"
+      ],
+      image: "img/TwitchAPI.png",
+      url: "https://raymeibaum.github.io/FCC-TwitchAPI"
+    }, {
+      title: "Wikipedia Viewer",
+      description: "Allows the user to search for Wikipedia articles.",
+      features: [
+        "Wikipedia Search API",
+        "Random article button",
+        "Bootstrap styling"
+      ],
+      image: "img/WikipediaViewer.png",
+      url: "https://raymeibaum.github.io/FCC-WikipediaViewer/"
+    }],
+    skills: [{
         category: "Strong",
-        items: ["HTML", "Bootstrap", "CSS",
-                "JavaScript", "jQuery", "Git/GitHub"]
-      },
-      {
+        items: [
+          "HTML",
+          "Bootstrap",
+          "CSS",
+          "JavaScript",
+          "jQuery",
+          "Git/GitHub"
+        ]
+      }, {
         category: "Experienced",
-        items: ["Python", "AngularJS", "MySQL", "Node.js", "Express"]
-      },
-      {
+        items: [
+          "Python",
+          "AngularJS",
+          "MySQL",
+          "Node.js",
+          "Express"
+        ]
+      }, {
         category: "Horizon",
-        items: ["ReactJS", "Angular2", "Aurelia", "Swift"]
+        items: [
+          "ReactJS",
+          "Angular2",
+          "Aurelia",
+          "Swift"
+        ]
       },
 
     ],
-    education: [
-      {
-        name: "Udacity",
-        description: "Full Stack Web Development Nanodegree, Computer Software Engineering",
-        year: "2016"
-      },
-      {
-        name: "Free Code Camp",
-        description: "Full Stack Web Development Certification, Computer Software Engineering",
-        year: "2015-2016"
-      },
-      {
-        name: "Operation Spark",
-        description: "Web Development Bootcamp, Computer Software Engineering",
-        year: "2015"
-      },
-      {
-        name: "Full Sail University",
-        description: "Bachelor of Art, Film",
-        year: "2008-2011"
-      },
-    ]
+    education: [{
+      name: "Udacity",
+      description: "Full Stack Web Development Nanodegree, Computer " +
+                   "Software Engineering",
+      year: "2016"
+    }, {
+      name: "Free Code Camp",
+      description: "Full Stack Web Development Certification, Computer " +
+                   "Software Engineering",
+      year: "2015-2016"
+    }, {
+      name: "Operation Spark",
+      description: "Web Development Bootcamp, Computer Software Engineering",
+      year: "2015"
+    }, {
+      name: "Full Sail University",
+      description: "Bachelor of Arts, Film",
+      year: "2008-2011"
+    }, ]
   };
 
   // Controller
@@ -107,6 +141,7 @@ $(function() {
   // Views
   var about = {
     init: function() {
+      this.aboutLead = $('#about-lead');
       this.aboutText = $('#about-text');
       this.aboutImage = $('#about-image');
       this.render();
@@ -114,8 +149,9 @@ $(function() {
     render: function() {
       var aboutText = controller.getAboutText();
       var aboutImage = controller.getAboutImage();
-      this.aboutText.text(aboutText);
-      this.aboutImage.attr('src',  aboutImage);
+      this.aboutLead.text(aboutText.lead);
+      this.aboutText.text(aboutText.main);
+      this.aboutImage.attr('src', aboutImage);
     }
   };
   var portfolio = {
@@ -126,8 +162,11 @@ $(function() {
     render: function() {
       var htmlStr = "";
       controller.getPortfolioArr().forEach(function(item, index) {
-        htmlStr += `<div class="col-sm-3"><a class="portfolio-item" id="${index}" data-toggle="modal" data-target="#portfolio-modal">
-                    <img class="img-responsive" src="${item.image}" alt="Project Image">
+        htmlStr += `<div class="col-sm-3"><a class="portfolio-item"
+                    id="${index}" data-toggle="modal"
+                    data-target="#portfolio-modal">
+                    <img class="img-responsive" src="${item.image}"
+                    alt="Project Image">
                     <h4 class="project-title">${item.title}</h4></a>
                     </div>`
       });
@@ -149,18 +188,20 @@ $(function() {
       this.modalBody.empty();
       var htmlStr = "";
       var portfolioItem = controller.getPortfolioItemById(id);
-      htmlStr += `<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                  <div class="row"><div class="col-sm-6"><a href="${portfolioItem.url}">
-                  <img class="img-responsive" src="${portfolioItem.image}" alt="Sample image from project"></a>
-                  </div><div class="col-sm-6" id="portfolio-modal-text">
+      htmlStr += `<button type="button" class="close" data-dismiss="modal"
+                  aria-label="Close"><span aria-hidden="true">&times;
+                  </span></button><div class="row"><div class="col-sm-6">
+                  <a href="${portfolioItem.url}"><img class="img-responsive"
+                  src="${portfolioItem.image}" alt="Sample image from project">
+                  </a></div><div class="col-sm-6" id="portfolio-modal-text">
                   <h3>${portfolioItem.title}</h3>
-                  <strong>Description</strong><p>${portfolioItem.description}</p>
-                  <strong>Features</strong><ul>`
+                  <strong>Description</strong><p>${portfolioItem.description}
+                  </p><strong>Features</strong><ul>`
       portfolioItem.features.forEach(function(feature) {
         htmlStr += `<li>${feature}</li>`
       });
-      htmlStr += `</ul><strong>Click <a href="${portfolioItem.url}">here</a> to view project.</strong>
-                  </div></div>`;
+      htmlStr += `</ul><strong>Click <a href="${portfolioItem.url}">here</a>
+                  to view project.</strong></div></div>`;
 
       this.modalBody.append(htmlStr);
     }
